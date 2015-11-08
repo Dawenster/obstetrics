b567276c6fce7093771fafe0b8094b6504b4da07
David Wen <dstwen@gmail.com>
2015-11-08 10:35:22 -0800

    Create and show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +12,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article successfully created!"
+      redirect_to articles_path
+    else
+      flash.now[:alert] = @article.errors.full_messages.to_sentence
+      render "new"
+    end
   end
 
   def edit
@@ -25,5 +32,12 @@ class ArticlesController < ApplicationController
 
   def destroy
     
+  end
+
+  def article_params
+    params.require(:article).permit(
+      :title,
+      :content
+    )
   end
 end
