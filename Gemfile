308b3a3218740fea5a3490736d4e548d51e30550
David Wen <dstwen@gmail.com>
2015-11-08 10:06:08 -0800

    Created articles#new page

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'aws-sdk', '~> 2'
 gem 'ckeditor'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jbuilder', '~> 2.0'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,29 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.order("title ASC")
+  end
+
+  def show
+    
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
     
   end
 end
