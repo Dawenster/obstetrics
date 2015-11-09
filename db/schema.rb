8976a71818399f6144b5fcbff8be8e7ed32b13e4
David Wen <dstwen@gmail.com>
2015-11-08 19:40:51 -0800

    Add row_order to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108174156) do
+ActiveRecord::Schema.define(version: 20151109034003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151108174156) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
