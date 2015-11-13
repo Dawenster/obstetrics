ccbdc2e3288dbf485d9f071f408c10dc01abc872
David Wen <dstwen@gmail.com>
2015-11-12 22:42:42 -0800

    Rank new articles first

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ActiveRecord::Base
   include RankedModel
   ranks :row_order
+
+  after_create :rank_as_first
+
+  def rank_as_first
+    self.update_attribute :row_order_position, 0
+  end
 end
