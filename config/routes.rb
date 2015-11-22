d88a915e829b7d3c80a5cf13134611dff358773e
David Wen <dstwen@gmail.com>
2015-11-21 19:00:51 -0800

    Update nav to dropdown style

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     post "drag-update" => "articles#drag_update", as: :drag_update
   end
 
-  resources :major_sections
-  resources :minor_sections
+  resources :major_sections, except: [:show]
+  resources :minor_sections, except: [:show]
 end
