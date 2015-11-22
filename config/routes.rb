c80142e079db6ac86c12690e2741667f61f7d275
David Wen <dstwen@gmail.com>
2015-11-21 18:55:57 -0800

    Add links to new major/minor sections

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :articles do
     post "drag-update" => "articles#drag_update", as: :drag_update
   end
+
+  resources :major_sections
+  resources :minor_sections
 end
